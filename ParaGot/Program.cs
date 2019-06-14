ad699a3175013837bb97eb648e19bcb4180a2bfa
Thiago Lanza <thlanza@gmail.com>
2019-06-14 17:07:59 -0300

    Projeto iniciado, mostrando a mensagem OlaMundo

---
+++ ParaGot/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ParaGot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo");
+            Console.ReadLine();
+        }
+    }
+}
